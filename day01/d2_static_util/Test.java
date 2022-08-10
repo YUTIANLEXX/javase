75e6b1ec53180920ba550b688e13f9cf100ab7c2
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-08-10 17:53:23 +0800

    Create Test.java

---
+++ day01/d2_static_util/Test.java
@@ -0,0 +1,13 @@
+package com.yutianle.d2_static_util;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = null;
+        int[] arr1 = {};
+        int[] arr2 = { 11, 22, 33, 44 };
+
+        System.out.println(ArrayUtil.toString(arr));
+        System.out.println(ArrayUtil.toString(arr1));
+        System.out.println(ArrayUtil.toString(arr2));
+    }
+}
