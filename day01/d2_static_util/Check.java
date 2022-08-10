27ab2bfa7e335a2466fb6f7d21a78b9ece9a6a3f
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-08-10 17:53:15 +0800

    Create Check.java

---
+++ day01/d2_static_util/Check.java
@@ -0,0 +1,8 @@
+package com.yutianle.d2_static_util;
+
+public class Check {
+    public static void main(String[] args) {
+        // 开发一个验证码：
+        System.out.println(ItUtil.createVerifyCode(6));
+    }
+}
