f9e56b720ba304b3b9e1bdd33ea948a4a7f0d73e
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-08-12 17:24:31 +0800

    Create Test.java

---
+++ day02/d1_package/Test.java
@@ -0,0 +1,22 @@
+package com.yutianle.d1_package;
+
+import com.yutianle.d1_package.it.Student;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        //目标：理解以下两点：
+        // 1、同一个包下的类，互相可以直接访问。
+        System.out.println(User.onlieNumber);
+
+        // 2、不同包下的类，必须先导包才可以访问。
+        Student s = new Student();
+        Scanner sc = new Scanner(System.in);
+
+        // 3、如果这个类中使用不同包下的相同的类名，
+        // 此时默认只能导入一个类的包，另一个类要使用全名访问。
+        com.yutianle.d1_package.it2.Student s2 = new com.yutianle.d1_package.it2.Student();
+
+    }
+}
